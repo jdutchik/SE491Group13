7003990fb93a2b1508d424e8642b00c44510c152
joshdutchik <77994722+jdutchik@users.noreply.github.com>
2023-11-29 15:12:42 -0600

    Login and Signup Setup

---
+++ se491project/components/SignAndLogin/SignAndLogin.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './SignAndLogin.css';
+
+
+const SignAndLogin = () => {
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'></div>
+                <div className='underline'></div>
+            </div>
+
+            <div className='userInput'>
+                <div className='input'>
+                    <img src="" alt="" />
+                    <input type="text" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignAndLogin

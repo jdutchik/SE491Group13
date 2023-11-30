3fed5352790d5ed919dd0005e1f31620a730def7
joshdutchik <77994722+jdutchik@users.noreply.github.com>
2023-11-30 00:44:09 -0600

    Login Changes

--- se491project/src/components/SignAndLogin/SignAndLogin.jsx
+++ se491project/src/components/SignAndLogin/SignAndLogin.jsx
@@ -9,29 +9,28 @@ const SignAndLogin = () => {
     return (
         <div className='container'>
             <div className='header'>
-                <div className='titleText'>Enter Creditionals</div>
+                <div className='titleText'>Enter Patient Creditionals</div>
             </div>
 
             <div className='inputs'>
                 <div className='userInput'>
                     <img src={userEmoji} alt="" />
-                    <input type="text" />
+                    <input type='text' placeHolder='Patient Name'/>
                 </div>
 
                 <div className='userInput'>
                     <img src={emailEmoji} alt=""/>
-                    <input type="email" />
+                    <input type='email' placeHolder='Patient Email'/>
                 </div>
 
                 <div className='userInput'>
                     <img src={passwordEmoji} alt="" />
-                    <input type="password" />
+                    <input type='password' placeHolder='Password'/>
                 </div>
             </div>
 
             <div className='submit'>
-                <div className="signUp">Sign Up</div>
-                <div className="Login">Login</div>
+                <div className="login">Login</div>
             </div>
         </div>
     )

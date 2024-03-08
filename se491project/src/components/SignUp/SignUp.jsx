120aa7b959228b306ca9afcad5219753469ff00a
joshdutchik <joshua.dutchik@gmail.com>
2024-03-07 23:14:38 -0600

    front end work

--- se491project/src/components/SignUp/SignUp.jsx
+++ se491project/src/components/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './SignUp.css';
 
 import sneezeImg from '../Assets/sneeze.jpg';
+import pass from '../Assets/passwordLock.png';
 
 const SignUp = () => {
     const signUpClicked = () => {
@@ -12,12 +13,18 @@ const SignUp = () => {
         <div className='signUpContainer'>
            <img src={sneezeImg}></img>
            <div className='SignUpInfo'>
-                <div className='signUpInfo'>Allergies Becoming A Problem? 
-                Experience peace of mind with our AI allergy detection service!
+                <div className='signUpInfo'> Are allergies putting a damper on your day? <br></br>
+                Do you find yourself constantly battling sneezing, hives, or red eyes? <br></br>
+                <br></br>
+                Find peace of mind with our new and exciting Artificial Intelligence 
+                allergy detection service!
                 Begin your journey by filling out a brief survey, and reach out to your doctor.
                 Simple! 
                 </div>
-                <div className="signUpButton" onClick={signUpClicked}>Sign Up</div>
+                <div className="fullButton">
+                    <img src={pass} alt="" />
+                    <div className="signUpButton" onClick={signUpClicked}>Sign Up For Free Today!</div>
+                </div>
            </div>
         </div>
     )

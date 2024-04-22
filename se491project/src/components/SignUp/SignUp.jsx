f613bee3422a1ea842bdbce845209237cf9b1203
joshdutchik <77994722+jdutchik@users.noreply.github.com>
2024-04-22 15:59:21 -0500

    finished sign up front end

--- se491project/src/components/SignUp/SignUp.jsx
+++ se491project/src/components/SignUp/SignUp.jsx
@@ -10,23 +10,23 @@ const SignUp = () => {
 
     return (
         <div className='signUpContainer'>
-           <div className='signUpInfo'>
                 <div className='intro-signup'> 
                     Are allergies putting a damper on your day? <br></br>
                     Do you find yourself constantly battling sneezing, hives, or red eyes? <br></br>
-                    Do you find yourself a victim of the WICKED Allergy Monster? <br></br>
+                    Do you find yourself a victim of the <span class="fun">WICKED</span> Allergy Monster? <br></br>
                 </div>
+
+                <div className="fullButton" onClick={signUpClicked}>
+                    <img src={signup} alt="" />
+                    <div className="signUpButton">Sign Up For Free Today!</div>
+                </div>
+
                 <div className='paragraph'> 
                     Find peace of mind with our new and exciting Artificial Intelligence 
                     allergy detection service!
                     Begin your journey by filling out a brief survey. Then all you need to do 
                     is get in contact with your doctor or physician for the results. It is that easy!
                 </div>
-           </div>
-           <div className="fullButton" onClick={signUpClicked}>
-                <img src={signup} alt="" />
-                <div className="signUpButton">Sign Up For Free Today!</div>
-            </div>
         </div>
     )
 }

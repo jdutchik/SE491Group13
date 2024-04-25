5de59872dd8a345efd881ce0802b648851083aec
jcgray2 <jackg@example.com>
2024-04-25 15:57:11 -0500

    updated symptom list. needs UI work

--- se491project/src/components/Survey/Survey.jsx
+++ se491project/src/components/Survey/Survey.jsx
@@ -1,27 +1,97 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Survey.css';
-
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
 import USAMap from "react-usa-map";
+
+const symptoms = [
+  "Sensitive skin allergist diagnosed",
+  "Sensitive skin self diagnosed",
+  "Allergic contact dermatitis",
+  "Eczema atopic skin",
+  "Dry chapped skin",
+  "Acne pimples",
+  "Skin allergies",
+  "Rosacea",
+  "Discoloration hyperpigmentation",
+  "Fine lines wrinkles",
+  "Psoriasis",
+  "I have celiacs disease and need my products to be gluten free",
+  "Blackheads whiteheads",
+  "Coconut",
+  "Textile Dye Mix",
+  "Gluten",
+  "Respiratory",
+  "Patchy rash",
+  "Tbd",
+  "Congestion",
+  "Contact dermatitis",
+  "Ffa",
+  "Dermatologist",
+  "Rash",
+  "Balsam Peru",
+  "I",
+  "Itching",
+  "Rashes",
+  "Mystery body rash",
+  "PPD",
+  "My gf gets rashes so I need to change my products",
+  "Alopecia",
+  "AP93",
+  "Dry skin",
+  "Cocamidopropyl Betaine",
+  "Metal",
+  "Ethylenediamine Dihydrochloride, Potassium Dichromate",
+  "Nickel",
+  "Low level allergy to Balsam of Peru",
+  "Allergies",
+  "Glutaral & iodopropynl butyl carbamate",
+  "Balsam of Peru allergic",
+  "Licus planus",
+  "Lip inflammation",
+  "Hives",
+  "Lichen Sclerosis",
+  "Dry Lips",
+  "Occasional rash outbreaks",
+  "Propolis",
+  "Allergic to Cocamidopropyl Betaine",
+  "Surgical site healing issues",
+  "Urticaria",
+  "Polysorbate 80",
+  "Fragrance",
+  "Contact Dermatitis",
+  "Red, peeling, itchy, burning lips",
+  "Celiac",
+  "Perioral dermatitis",
+  "Scalp",
+  "Scalp issues",
+  "Hives on legs",
+  "Itchy scalp with hair loss",
+  "Allergic reaction",
+  "Excema",
+  "Dermatitis Herpetiformis (dermatologist diagnosed)",
+  "Allergic to polyethylene glycol (peg)",
+  "Oral lichen Plans"
+];
 
 const Survey = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [state, setState] = useState(null);
 
-  // patient name
-  const [email, setEmail] = useState(null);
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [full_name, setFull_Name] = useState(null);
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [full_name, setFull_Name] = useState('');
 
-  // everything else
   const [gender, setGender] = useState(null);
   const [skin_tone, setSkin_Tone] = useState(null);
-  const [symptoms, setSymptoms] = useState(null);
   
+  const [symptomList, setSymptomList] = useState(symptoms.map(symptom => ({
+    name: symptom,
+    isSelected: false
+  })));
+
   const years = [];
   for (let year = 1960; year <= 2024; year++) {
     years.push(year);
@@ -42,65 +112,41 @@ const Survey = () => {
     "December",
   ];
 
+  const toggleSymptomSelection = (index) => {
+    const newSymptoms = [...symptomList];
+    newSymptoms[index].isSelected = !newSymptoms[index].isSelected;
+    setSymptomList(newSymptoms);
+  };
+
   const mapHandler = (event) => {
     const event_state = event.target.dataset.name;
     setState(event_state);
   };
+  
   const navigate = useNavigate();
 
   const submitClicked = () => {
     window.location.href = '/';
   };
 
-  const [userData, setUserData] = useState({
-    username: '',
-    password: '',
-    email: ''
-  });
-
-  const [patientData, setPatientData] = useState({
-    name: '',
-    gender: '',
-    skin_tone: ''
-  });
-
-  const [dob, setDob] = useState({
-    day: '',
-    month: '',
-    year: ''
-  });
-
-  const handleUserInputChange = (e) => {
-    const { name, value } = e.target;
-    setUserData({ ...userData, [name]: value });
-  };
-
-  const handlePatientInputChange = (e) => {
-    const { name, value } = e.target;
-    setPatientData({ ...patientData, [name]: value });
-  };
-
-  const handleDobChange = (e) => {
-    const { name, value } = e.target;
-    setDob({ ...dob, [name]: value });
-  };
-
   const handleSurveySubmit = async (e) => {
     e.preventDefault();
 
     const dob_to_string = startDate.toDateString().split(" ");
-    const dob_sql = dob_to_string[3] + '-' + (startDate.getMonth()+1) + '-' + dob_to_string[2];
+    const dob_sql = dob_to_string[3] + '-' + (months.indexOf(dob_to_string[1]) + 1).toString().padStart(2, '0') + '-' + dob_to_string[2];
+
+    const selectedSymptoms = symptomList.filter(symptom => symptom.isSelected).map(symptom => symptom.name).join(', ');
 
     const completePatientData = {
-      "email" : email,
-      "name" : full_name,
-      "username" : username,
-      "password" : password,
-      "dob" : dob_sql,
-      "gender" : gender,
-      "state" : state,
-      "skin_tone" : patientData[skin_tone],
-      "symptoms" : "Dry chapped skin"
+      "email": email,
+      "name": full_name,
+      "username": username,
+      "password": password,
+      "dob": dob_sql,
+      "gender": gender,
+      "state": state,
+      "skin_tone": skin_tone,
+      "symptoms": selectedSymptoms
     };
 
     try {
@@ -122,62 +168,52 @@ const Survey = () => {
       console.error('Error:', error);
     }
   };
+
   return (
     <div className="surveyContainer">
       <form className="userForm" onSubmit={handleSurveySubmit}>
         <div className="userDebrief">
           <h1>Welcome! Let's get started 👋</h1>
           <p className="intro">
-            To ensure accurate determination of allergies,
-            it is crucial for users to input correct and comprehensive information.
-            Please provide precise details about the patient's name, age, gender, race, weight, diet,
-            and any relevant geographical information. Accurate data enables our Artificial
-            Intelligence model to come to a more informed
-            conclusion on the patient's potential allergies. Additionally,
-            if you have a change in any of the listed info in the future, please consider
-            updating this information to help deliver personalized and reliable results.
-            Your cooperation in providing accurate
-            information plays a pivotal role in optimizing the effectiveness of the allergy
-            determination process.
+            To ensure accurate determination of allergies, it is crucial for users to input correct and comprehensive information.
+            Please provide precise details about the patient's name, age, gender, race, weight, diet, and any relevant geographical information. Accurate data enables our Artificial Intelligence model to come to a more informed conclusion on the patient's potential allergies. Additionally, if you have a change in any of the listed info in the future, please consider updating this information to help deliver personalized and reliable results.
+            Your cooperation in providing accurate information plays a pivotal role in optimizing the effectiveness of the allergy determination process.
           </p>
         </div>
 
         <div className="questions">
-
-          {/* Name input field */}
           <div className="first-six">
             <div className="user-info">
               <div className="basic">
                 <h>Email</h>
-                <input type="text" placeholder="example@gmail.com" onChange={({ target: { value } }) => setEmail(value)}/>
+                <input type="text" placeholder="example@gmail.com" value={email} onChange={({ target: { value } }) => setEmail(value)} />
               </div>
 
               <div className="basic">
                 <h>Username</h>
-                <input type="text" placeholder="Enter Username" onChange={({ target: { value } }) => setUsername(value)}/>
+                <input type="text" placeholder="Enter Username" value={username} onChange={({ target: { value } }) => setUsername(value)} />
               </div>
 
               <div className="basic">
                 <h>Password</h>
-                <input type="text" placeholder="Enter Password" onChange={({ target: { value } }) => setPassword(value)}/>
+                <input type="text" placeholder="Enter Password" value={password} onChange={({ target: { value } }) => setPassword(value)} />
               </div>
             </div>
 
-            <div class="patient-info">
-              {/* Age input field */}
+            <div className="patient-info">
               <div className="name-gender">
                 <div className="name">
                   <h>Full Patient Name</h>
-                  <input type="text" placeholder="Enter Full Patient Name" onChange={({ target: { value } }) => setFull_Name(value)}/>
+                  <input type="text" placeholder="Enter Full Patient Name" value={full_name} onChange={({ target: { value } }) => setFull_Name(value)} />
                 </div>
 
-                {/* gender input field */}
                 <div className="gender">
                   <h>Gender</h>
-                  <select className="genderInput" onChange={({ target: { value } }) => setGender(value)}>
-                    <option>Male</option>
-                    <option>Female</option>
-                    <option>Other</option>
+                  <select className="genderInput" value={gender} onChange={({ target: { value } }) => setGender(value)}>
+                    <option value="">Select Gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
                   </select>
                 </div>
               </div>
@@ -187,28 +223,17 @@ const Survey = () => {
                 <DatePicker
                   showIcon
                   inline
-                  renderCustomHeader={({ date,
-                    changeYear,
-                    changeMonth }) => (
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "center",
-                      }}
-                    >
+                  renderCustomHeader={({ date, changeYear, changeMonth }) => (
+                    <div style={{ display: "flex", justifyContent: "center" }}>
                       <select value={date.getFullYear()} onChange={({ target: { value } }) => changeYear(value)}>
                         {years.map((option) => (
-                          <option key={option} value={option}>
-                            {option}
-                          </option>
+                          <option key={option} value={option}>{option}</option>
                         ))}
                       </select>
 
                       <select value={months[date.getMonth()]} onChange={({ target: { value } }) => changeMonth(months.indexOf(value))}>
                         {months.map((option) => (
-                          <option key={option} value={option}>
-                            {option}
-                          </option>
+                          <option key={option} value={option}>{option}</option>
                         ))}
                       </select>
                     </div>
@@ -225,80 +250,47 @@ const Survey = () => {
             <USAMap onClick={mapHandler} customize={{ [state]: { fill: 'navy' } }}/>
           </div>
 
-          {/* skin ton input field*/}
           <div className="skintone">
             <h>Skin Tone</h>
             <div className="options">
               <label>
-                <div class="dark"></div>
-                <input type="radio" name="skin_tone" value="Dark" onChange={handlePatientInputChange} checked={patientData.skin_tone === 'Dark'} /> Dark
+                <input type="radio" name="skin_tone" value="Dark" onChange={({ target: { value } }) => setSkin_Tone(value)} checked={skin_tone === 'Dark'} /> Dark
               </label>
-
               <label>
-                <div class="brown"></div>
-                <input type="radio" name="skin_tone" value="Brown" onChange={handlePatientInputChange} checked={patientData.skin_tone === 'Brown'} /> Brown
+                <input type="radio" name="skin_tone" value="Brown" onChange={({ target: { value } }) => setSkin_Tone(value)} checked={skin_tone === 'Brown'} /> Brown
               </label>
-
               <label>
-                <div class="olive"></div>
-                <input type="radio" name="skin_tone" value="Olive" onChange={handlePatientInputChange} checked={patientData.skin_tone === 'Olive'} /> Olive
+                <input type="radio" name="skin_tone" value="Olive" onChange={({ target: { value } }) => setSkin_Tone(value)} checked={skin_tone === 'Olive'} /> Olive
               </label>
-
               <label>
-                <div class="medium"></div>
-                <input type="radio" name="skin_tone" value="Medium" onChange={handlePatientInputChange} checked={patientData.skin_tone === 'Medium'} /> Medium
+                <input type="radio" name="skin_tone" value="Medium" onChange={({ target: { value } }) => setSkin_Tone(value)} checked={skin_tone === 'Medium'} /> Medium
               </label>
-
               <label>
-                <div class="fair"></div>
-                <input type="radio" name="skin_tone" value="Fair" onChange={handlePatientInputChange} checked={patientData.skin_tone === 'Fair'} /> Fair
+                <input type="radio" name="skin_tone" value="Fair" onChange={({ target: { value } }) => setSkin_Tone(value)} checked={skin_tone === 'Fair'} /> Fair
               </label>
-
               <label>
-                <div class="light"></div>
-                <input type="radio" name="skin_tone" value="Light" onChange={handlePatientInputChange} checked={patientData.skin_tone === 'Light'} /> Light
+                <input type="radio" name="skin_tone" value="Light" onChange={({ target: { value } }) => setSkin_Tone(value)} checked={skin_tone === 'Light'} /> Light
               </label>
             </div>
           </div>
 
-          {/* other input field*/}
+          {/* Skin Symptoms Section */}
           <div className="other">
-            <h>Experiencing Any Unusual Symptoms?</h>
+            <h>Skin Symptoms</h>
             <div className="other-options">
-              <input type="checkbox" name="ssad" value="ssad"></input>
-              <label for="ssad"> Sensitive Skin (allergist diagnosed)</label><br></br>
-
-              <input type="checkbox" name="sssd" value="sssd"></input>
-              <label for="sssd"> Sensitive Skin (self diagnosed)</label><br></br>
-
-              <input type="checkbox" name="acd" value="acd"></input>
-              <label for="acd"> Allergic Contact Dermatitis</label><br></br>
-
-              <input type="checkbox" name="eas" value="eas"></input>
-              <label for="eas"> Eczema Atopic Skin</label><br></br>
-
-              <input type="checkbox" name="dcs" value="dcs"></input>
-              <label for="dcs"> Dry Chapped Skin</label><br></br>
-
-              <input type="checkbox" name="ap" value="ap"></input>
-              <label for="ap"> Acne Pimples</label><br></br>
-
-              <input type="checkbox" name="sa" value="sa"></input>
-              <label for="sa"> Skin Allergies</label><br></br>
-
-              <input type="checkbox" name="ros" value="ros"></input>
-              <label for="ros"> Rosacea</label><br></br>
-
-              <input type="checkbox" name="dh" value="dh"></input>
-              <label for="dh"> Discoloration Hyperpigmentation</label><br></br>
-
-              <input type="checkbox" name="flw" value="flw"></input>
-              <label for="flw"> Fine Lines Wrinkles</label><br></br>
-
-              <input type="checkbox" name="pso" value="pso"></input>
-              <label for="pso"> Psoriasis</label><br></br>
+              {symptomList.map((symptom, index) => (
+                <div key={index}>
+                  <input
+                    type="checkbox"
+                    checked={symptom.isSelected}
+                    onChange={() => toggleSymptomSelection(index)}
+                  />
+                  <label>{symptom.name}</label><br/>
+                </div>
+              ))}
             </div>
           </div>
+
           <button type="submit" className="surveySubmit" onClick={submitClicked}>Submit</button>
         </div>
       </form>
@@ -307,3 +299,4 @@ const Survey = () => {
 };
 
 export default Survey;
+

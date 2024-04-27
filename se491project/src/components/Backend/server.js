5f7a1af191a46df1194f7f88050af4280f15f925
joshdutchik <77994722+jdutchik@users.noreply.github.com>
2024-04-27 11:52:49 -0500

    update server no crash

--- se491project/src/components/Backend/server.js
+++ se491project/src/components/Backend/server.js
@@ -81,6 +81,11 @@ app.get('/products/:username', (req, res) => {
     }
 
     const ingredients = patient[0].ingredients;
+
+    if (ingredients.length == 0) {
+      return res.status(404).json({ error: 'No Ingredients' });
+    }
+
     const split_ing = ingredients.split(';');
 
     connection.query('SELECT * FROM products', (error, products) => {
